59a1420b840e5646d8372207ec42624f532056eb
Ben Newton <ben.newton112@gmail.com>
2023-07-29 08:54:23 +0100

    filters done and working

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -11,78 +11,67 @@ function Homepage() {
   
   useEffect(
     ()=>{
-    //API call for product data
-    axios.get(`https://fakestoreapi.com/products/${filterCategory}`)
+      //API call for categories
+  axios.get(`https://fakestoreapi.com/products/categories`)
+  .then(res => {
+    setCategory(res.data)
+  })
+  .catch(err => console.log(err))
+
+    //API call for app products - when page loads
+    axios.get(`https://fakestoreapi.com/products/`)
     .then(res => {
     //Storing data in State
     setProducts(res.data)
     console.log(res.data)
     })
     .catch(err => console.log(err))
-  }, []
-  )
 
-    //onClick formula to grab filter name
-    const filterSelectChange = (value) => {
+}, []
+)
+
+  //onClick formula to grab filter name
+  const filterSelectChange = (value) => {
   
-    console.log(value.target.id)
-    setFilterCategory(value.target.id)
-    console.log(setFilterCategory)
-    }
-
-    //API call for Category names
-    useEffect(
-        ()=>{
-          axios.get(`https://fakestoreapi.com/products/categories/`)
-          .then(res => {
-            setCategory(res.data)
-          })
-          .catch(err => console.log(err))
-          
-        }, []
-      )
-
-    useEffect(
-      ()=>{
-
-
-        fetchFilters()
-      }, [filterCategory]
-    )
-
-    //API call for specific Category products
-
-    const fetchFilters = async (onClick) => {
-      try{
-
-      setFilterCategory()
-      console.log(`hey`,filterCategory)
-      }
-      catch (err){
-        console.log(err)
-      }
+  console.log(value.target.id)
+  setFilterCategory(value.target.id)
 }
 
+  useEffect(
+    ()=>{
+      //API call to fetch filtered products
+    const url = filterCategory==="all"?
+    "https://fakestoreapi.com/products"
+    :
+    `https://fakestoreapi.com/products/category/${filterCategory}`
 
+    axios.get(url)
+    .then(res =>{
+    console.log(res.data)
+    setProducts(res.data)
+})
+    .catch(err => console.log(err))
 
+    }, [filterCategory]
+)
 
   return (
     <div className='homepage-container'>
 
 <div className='category-container'>
 
-<button id=''onClick={filterSelectChange}>All</button>
-<button id={"category/electronics"}onClick={filterSelectChange}>{category[0]}</button>
-<button id={"category/jewelery"}onClick={filterSelectChange}>{category[1]}</button>
-<button id={"category/men'sclothing"}onClick={filterSelectChange}>{category[2]}</button>
-<button id={"category/womens'sclothing"} onClick={filterSelectChange}>{category[3]}</button>
+<button id='all'onClick={filterSelectChange}>All</button>
+<button id={"electronics"}onClick={filterSelectChange}>{category[0]}</button>
+<button id={"jewelery"}onClick={filterSelectChange}>{category[1]}</button>
+<button id={"men's clothing"}onClick={filterSelectChange}>{category[2]}</button>
+<button id={"women's clothing"} onClick={filterSelectChange}>{category[3]}</button>
 
 
 </div>
 
     <div className='product-container'>
       {
-        products.map(item => <Product_Detail
+        products && products.map(item => <Product_Detail
         key={item.id} product={item} />)
       }
 
