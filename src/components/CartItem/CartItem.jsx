26d6ba510418db22e033175bfb24adf5fce2aa03
Ben Newton <ben.newton112@gmail.com>
2023-07-30 13:02:58 +0100

    checkout flex done

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -11,10 +11,10 @@ const {removeCart} = useContext(CartContext)
   return (
     <div className='cart-items'>
     <img src={product.image} />
-    <h3 style={{width: 200}}>{product.title}</h3>
+    <h3 style={{width:100}}>{product.title}</h3>
     <h3>£{product.price}</h3>
-    <h3>1</h3>
-    <h3 onClick={() => removeCart(product)}><MdDelete /></h3>
+    <h3 style={{marginLeft: 60}}>1</h3>
+    <h3 style={{marginLeft: 40}} onClick={() => removeCart(product)}><MdDelete /></h3>
 
     </div>
   )
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -41,14 +41,15 @@ function Cart({}) {
             <div className='cart-box'>
             <div className='cart-titles'>
                 <div className='cart-item'>
-                    <p style={{marginRight: 150}}>Item</p>
+                    <p style={{marginRight: 0}}>Item</p>
                 </div>
                 <div className='cart-info'>
-                    <p style={{marginLeft: 180}}>Price</p>
+                    <p style={{marginLeft: 160}}>Price</p>
                     <p style={{marginLeft: 30}}>Quantity</p>
                     <p style={{marginLeft: 20}}>Remove</p>
                 </div>
                 </div>
+                <div className='item-and-total-container'>
                 <div className='cart-item-container'>
                 {cart.length > 0 ? (
                     cart.map(item => <CartItem key={item.id} product={item} />)
@@ -78,6 +79,7 @@ function Cart({}) {
         </div>
       </Modal>
 
+            </div>
             </div>
         </div>
         </div>
